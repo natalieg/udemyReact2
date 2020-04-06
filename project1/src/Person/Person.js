42f3837392ad1b4d447e489109d684b6b7c78a2b
Natalie <natalie.gornicki@gmail.com>
2020-04-06 13:05:28 +0200

    changed folder structure

---
+++ project1/src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const person = (props) => {
+    return (
+        <div>
+            <p className='person' onClick={props.click}>
+                My name is {props.name} <br />
+                I am {props.age} years old!<br/>
+                <input type='text' onChange={props.changed} value={props.name}/>
+            </p>
+            <p>{props.children}</p>
+        </div>
+    )
+};
+
+export default person;
