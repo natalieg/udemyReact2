5f508e54600f9824308d381f83dabe27c9a49cfb
Natalie <natalie.gornicki@gmail.com>
2020-04-20 19:25:34 +0200

    side menu

--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,16 +2,28 @@ import React from 'react'
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Aux from '../../../hoc/Auxi'
 
 function toolbar(props) {
     return (
-        <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo/>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </header>
+        <Aux>
+            <div onClick={props.showSide}
+                style={{ 
+                    height: "40px", 
+                    padding: "10px", 
+                    position: "absolute",
+                    top: "0",
+                    right: "0" }}>
+                <Logo /></div>
+            <header className={[classes.Toolbar, classes.DesktopOnly].join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </header>
+        </Aux>
     )
 }
 
